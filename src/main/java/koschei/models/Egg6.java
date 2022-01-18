1e45627ed974f362ec6ef4370f95ffc4c62c96ff
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:51:01 +0300

    pom update

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,22 @@
 package koschei.models;
 
-public class Egg6 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+
+
+public class Egg6 {
+    private Needle7 nb;
+    public Egg6(Needle7 nb){
+        this.nb = nb;
+    }
+    @Autowired
+    public void setNb(Needle7 nb){
+        this.nb = nb;
+    }
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + nb.toString();
     }
+
 }
